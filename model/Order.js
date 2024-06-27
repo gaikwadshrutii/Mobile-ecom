ea2238238050011ed809ffd5b50599baff75f762
gaikwadshrutii <gaikwadshrutii01@gmail.com>
2024-06-27 11:59:51 +0530

    server setup

---
+++ model/Order.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose")
+
+const orderSchema = new mongoose.Schema({
+    user: { type: mongoose.Types.ObjectId, ref: "user", required: true },
+    products: [
+        {
+            product: {
+                type: mongoose.Types.ObjectId,
+                ref: "product"
+            },
+            qty: {
+                type: Number,
+                required: true
+            },
+        }
+    ],
+    status: { type: String, enum: ["placed", "cancel", "delivered"], default: "placed" },
+}, { timestamps: true })
+module.exports = mongoose.model("order", orderSchema)
